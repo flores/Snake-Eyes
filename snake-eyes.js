da7b85e42905a644ef3f087c479c8ea55cc0f042
Ryan Wolf <rwolf@borderstylo.com>
2010-12-21 13:04:53 -0800

    bigger, better. snakier?

--- snake-eyes.js
+++ snake-eyes.js
@@ -1,23 +1,36 @@
-requires.paths.unshift('vendor/node-irc/lib');
+require.paths.unshift('vendor/node-irc/lib');
 
 var Client = require('irc').Client;
 var http = require('http');
 
+
 var irc = new Client('eng.borderstylo.int', 'SnakeEyes', { channels: [ '#clients', '#rd' ]});
 
-irc.on('motd', function () {
-http.createServer(function (req, res) {
+var httpHandler = function (req, res) {
   req.setEncoding('utf8');
 
+  // on POSTs to /room, say body in #room
   req.on('data', function (data) {
     var room = req.url.replace(/\//, '');
     irc.say('#' + room, '\x032' + data);
   });
-  req.on('end', function () {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-  });
-:}).listen(8124);
+  req.on('end', function () { res.writeHead(204); res.end(); });
+};
+
+irc.on('motd', function () {
+  // don't start listening for http traffic until connected on irc
+  http.createServer(httpHandler).listen(8124);
 });
 
-
+irc.on('message', function (nick, to, text) {
+  if (/^#/.test(to) && /^SnakeEyes:/.test(text)) {
+    // general handling of messages to snakeeyes in a channel
+    switch (text) {
+      case 'SnakeEyes: reload':
+        irc.say(to, 'todo');
+        break;
+      default:
+        irc.say(to, 'willis: what are you talking about?');
+    }
+  }
+});
